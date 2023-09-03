2901a94c9382a9faf72a31d24a65519f179c365b
Princess Konwea <princesschi22@gmail.com>
2023-09-03 13:48:14 +0200

    added new sql codes

--- employees_database/employees' database.sql
+++ employees_database/employees' database.sql
@@ -244,5 +244,135 @@ SELECT
 FROM
     RankedSalaries
 WHERE
+dense_salary_rank IN (1, 2, 3);
+
+/*Find the employees who resigned from the company in the last 4 months.
+--Interested columns: employee_name, churn_date and reason for churn.*/
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+    ec.churn_date,
+    ec.reason
+FROM employeechurn ec
+JOIN employees e ON ec.emp_no = e.emp_no
+WHERE ec.churn_date >= DATE_TRUNC('MONTH', CURRENT_DATE - INTERVAL '4 MONTHS')
+AND ec.reason iLIKE '%resign%'
+
+/*Find the oldest employee in each department. I am interested in seeing the number of years spent with the company  as tenure, employee_name, 
+--First and last name should be in one column as employee_name, department, and employee_id.*/
+WITH employees AS (
+	SELECT *,
+	current_date - hire_date as tenure_in_days
+	FROM employees
+),
+oldest_employees AS (
+    SELECT
+        d.dept_name,
+        CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+		e.emp_no,
+        ROW_NUMBER() OVER(PARTITION BY dept_name ORDER BY tenure_in_days DESC) AS rank
+    FROM employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+	JOIN departments d ON de.dept_no = d.dept_no
+	LEFT JOIN employeechurn ec ON e.emp_no = ec.emp_no
+	WHERE churn_date IS NULL
+)
+
+SELECT * from oldest_employees
+WHERE rank = 1
+
+/*What is the number of employees compared to employees in each department*/
+
+SELECT d.dept_name, SUM(s.salary)
+	OVER()
+FROM departments d
+	JOIN dept_emp de
+	ON d.dept_no = de.dept_no
+	JOIN employees e
+	ON de.emp_no = e.emp_no
+	JOIN salaries s
+	ON s.emp_no = e.emp_no
+
+/*Rank the top earning departments and rank based top earners using 1-10*/
+SELECT d.dept_name, 
+	SUM(s.salary) AS total_sum,
+	RANK()
+	OVER(ORDER BY SUM(s.salary) DESC) AS salary_rank
+FROM departments d
+JOIN dept_emp de 
+ON d.dept_no = de.dept_no
+JOIN employees e 
+ON de.emp_no = e.emp_no
+JOIN salaries s 
+ON s.emp_no = e.emp_no
+GROUP BY d.dept_name
+
+/* Show the top 3 highest paid employees in each department*/				 
+WITH RankedSalaries AS (
+    SELECT
+        e.first_name,
+        e.last_name,
+        d.dept_name,
+        MAX(s.salary) AS total_salary,
+        DENSE_RANK() OVER (PARTITION BY d.dept_name ORDER BY MAX(s.salary) DESC) AS dense_salary_rank
+    FROM departments d
+    JOIN dept_emp de ON d.dept_no = de.dept_no
+    JOIN employees e ON de.emp_no = e.emp_no
+    JOIN salaries s ON s.emp_no = e.emp_no
+    GROUP BY d.dept_no, e.first_name, e.last_name, d.dept_name
+)
+SELECT
+    first_name,
+    last_name,
+    dept_name,
+    total_salary,
+    dense_salary_rank
+FROM
+    RankedSalaries
+WHERE
 dense_salary_rank IN (1, 2, 3);					 
+					 
+/*Find the average number of years that the employees stayed in each department before they churned*/.
+SELECT 
+    d.dept_name,
+    CASE
+        WHEN subquery.has_churned = 0 THEN 'No_churned_employee'
+        ELSE AVG(subquery.avg_years)::TEXT
+    END AS department_average
+FROM departments d
+LEFT JOIN (
+    SELECT
+        d.dept_name,
+        AVG(EXTRACT(YEAR FROM ec.churn_date) - EXTRACT(YEAR FROM e.hire_date)) AS avg_years,
+        COUNT(ec.emp_no) AS has_churned
+    FROM departments d
+    JOIN dept_emp de ON d.dept_no = de.dept_no
+    JOIN employees e ON de.emp_no = e.emp_no
+    LEFT JOIN employeechurn ec ON e.emp_no = ec.emp_no
+    GROUP BY d.dept_name
+) subquery ON d.dept_name = subquery.dept_name
+GROUP BY d.dept_name, subquery.has_churned;
+
+/*Which department has the highest churn rate (in percentage)*/
+
+SELECT 
+	d.dept_name,
+	count(*) as total_no_churned,
+	concat(round(count(*) * 100.0 / (select count(*) from employeechurn),2), '%') AS percentage_churned
+FROM departments d
+JOIN dept_emp de on d.dept_no = de.dept_no
+JOIN employees e ON de.emp_no = e.emp_no
+ JOIN employeechurn ec ON e.emp_no = ec.emp_no
+GROUP BY d.dept_name;
+	
+---Highest churn considering the count percentage of churned employees against the entire employee table
+SELECT 
+	d.dept_name,
+	count(*) as total_no_churned,
+	concat(round(count(*) * 100.0 / (select count(*) from employees), 4), '%') AS percentage_churned
+FROM departments d
+JOIN dept_emp de on d.dept_no = de.dept_no
+JOIN employees e ON de.emp_no = e.emp_no
+ JOIN employeechurn ec ON e.emp_no = ec.emp_no
+GROUP BY d.dept_name;
+
 					 
